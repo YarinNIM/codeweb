26032ecb21f64a54dd275933f477f1ac420e81cf
Yarin NIM <yarin097.infinity@gmail.com>
2024-03-12 20:22:07 +0700

    updte

--- apps/codeweb-web/src/Main.tsx
+++ apps/codeweb-web/src/Main.tsx
@@ -1,4 +1,26 @@
-const timer = window.setInterval(() => {
-  const now = new Date().getTime();
-  console.log({ now });
-}, 1000);
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate() {
+	requestAnimationFrame( animate );
+
+	cube.rotation.x += Math.random() * 0.1;
+	cube.rotation.y += Math.random() * 0.1;
+
+	renderer.render( scene, camera );
+}
+
+animate();
